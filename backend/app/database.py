afe09622f9547e89911fb1767a118f19a6df25f4
taekyun1212 <dlxorbs011@gmail.com>
2026-01-31 14:57:52 +0900

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "mysql+pymysql://admin:00000000@database-1.cno4c0skmxe1.ap-northeast-2.rds.amazonaws.com:3306/mydb"
+DB_URL = os.getenv("DB_URL") 
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
